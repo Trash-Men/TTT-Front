d9d7aa6c60158cee47eaa6808964b1c559e8378d
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-07 21:41:26 +0900

    add : login for DMSAuth

---
+++ pages/login/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const StatisticsPage = () => {
+  return (
+    <>
+      <Head>
+        <title>TTT | Login</title>
+      </Head>
+      <div>login</div>
+      <a href="http://localhost:3002/login">DMS로 로그인하기</a>
+    </>
+  );
+};
+
+export default StatisticsPage;
